39879f5212e59a7509684c40ad88de39d19f6b78
Aaron Li <aaronli39@gmail.com>
2018-10-09 21:55:38 -0400

    finished

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "linked_list.c"
+
+int main() {
+    srand(time(NULL));
+    struct node * head = create(1);
+    printf("              ==> HEAD node is: 1 <==\n");
+    int i = 4;
+    printf("============== TESTING insert_front ==============\n");
+    while(i--) {
+        insert_front(head, rand());
+        print_list(head);
+    }
+    printf("================ TESTING free_list ================\n");
+    printf("--> free_list worked because the first node is a null element so there <--\n");
+    printf("--> is a segmentation fault error since the previous elements are gone. <--\n\n");
+    struct node * temp = free_list(head);
+    print_list(temp);
+
+}
